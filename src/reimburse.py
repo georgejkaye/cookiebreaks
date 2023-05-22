b49fd7afdac1fd3d9468d74a73db6c58f9f5e263
George Kaye <georgejkaye@gmail.com>
2023-05-22 22:36:51 +0100

    [feature] Add script to reimburse hosts

---
+++ src/reimburse.py
@@ -0,0 +1,27 @@
+from config import parse_config
+from database import reimburse_and_mask_host
+from interactive import select_break
+
+
+def main():
+    config = parse_config()
+    chosen_break = select_break(config, past=True, hosted=True)
+    if chosen_break is None:
+        print("No choice made, exiting")
+        exit(0)
+    else:
+        host = chosen_break.host
+        cost = "{:.2f}".format(chosen_break.cost)
+        date = chosen_break.get_break_datetime()
+        check = input((
+            f"Have you reimbursed {host} £{cost} for hosting cookie break on {date}? (y/N) "
+        ))
+        if not check == "y":
+            print("Aborting...")
+            exit(0)
+        else:
+            reimburse_and_mask_host(config, chosen_break.id)
+
+
+if __name__ == "__main__":
+    main()
