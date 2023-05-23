6cdc9cb1c6d5d22cc2a37c60457e5b731019bbc9
George Kaye <georgejkaye@gmail.com>
2023-05-23 23:36:07 +0100

    [feature] Add input for cost when reimbursing host

--- src/reimburse.py
+++ src/reimburse.py
@@ -1,16 +1,22 @@
 from config import parse_config
 from database import reimburse_and_mask_host
 from interactive import select_break
-from structs import format_as_price
+from structs import BreakFilters, format_as_price
 
 
 def main():
     config = parse_config()
-    chosen_break = select_break(config, past=True, hosted=True)
+    chosen_break = select_break(config, BreakFilters(
+        past=True, hosted=True, host_reimbursed=False))
     if chosen_break is None:
         print("No choice made, exiting")
         exit(0)
     else:
+        cost = None
+        while cost is None:
+            candidate_cost = input("Break cost: ")
+            if candidate_cost.isnumeric():
+                cost = float(candidate_cost)
         host = chosen_break.host
         cost = format_as_price(chosen_break.cost)
         date = chosen_break.get_break_datetime()
