5b0072161c0d8bed46871bb29189b0897967da02
George Kaye <georgejkaye@gmail.com>
2023-08-09 23:12:22 +0100

    [impl] Remove pydantic tat from internal structs

--- api/src/structs.py
+++ api/src/structs.py
@@ -1,50 +1,25 @@
-import dataclasses
-from dataclasses import field
+from dataclasses import dataclass
 
 from typing import List, Optional
+from unittest.mock import Base
 from arrow import Arrow
 
-from pydantic import BaseConfig, ConfigDict
-from pydantic.dataclasses import dataclass
-
-BaseConfig.arbitrary_types_allowed = True
 
 def format_as_price(cost: float) -> str:
     return f"£{cost:.2f}"
 
 
-def format_as_iso8601(a: Arrow) -> str:
-    return a.format("YYYY-MM-dd[T]HH:mm:ss[Z]ZZ")
-
-
-def format_as_maybe_iso8601(a: Optional[Arrow]) -> Optional[str]:
-    if a is None:
-        return None
-    return format_as_iso8601(a)
-
-
-class BreakConfig:
-    json_encoders = {Arrow: lambda x: x.isoformat()}
-    arbitrary_types_allowed = True
-
-
-@dataclass(config=BreakConfig)
+@dataclass
 class Break:
-    id: int = field(metadata=dict(example=21))
-    break_time: Arrow = field(metadata=dict(example="2023-07-05T15:00:00+01:00"))
-    location: str = field(metadata=dict(example="LG06a, Computer Science"))
-    holiday: bool = field(metadata=dict(example=False))
-    host: Optional[str] = field(default=None, metadata=dict(example="George Kaye"))
-    cost: Optional[float] = field(default=None, metadata=dict(example=9.8))
-    host_reimbursed: Optional[Arrow] = field(
-        default=None, metadata=dict(example="2023-07-05T16:00+01:00")
-    )
-    admin_claimed: Optional[Arrow] = field(
-        default=None, metadata=dict(example="2023-07-26T16:45:00+01:00")
-    )
-    admin_reimbursed: Optional[Arrow] = field(
-        default=None, metadata=dict(example="2023-08-04T09:00:00+01:00")
-    )
+    id: int
+    break_time: Arrow
+    location: str
+    holiday: bool
+    host: Optional[str] = None
+    cost: Optional[float] = None
+    host_reimbursed: Optional[Arrow] = None
+    admin_claimed: Optional[Arrow] = None
+    admin_reimbursed: Optional[Arrow] = None
 
     def get_break_time(self) -> str:
         return self.break_time.format("HH:mm")
@@ -58,19 +33,6 @@ class Break:
     def get_break_datetime(self) -> str:
         return f"{self.get_break_date()} @ {self.get_break_time()}"
 
-    def to_json(self):
-        return {
-            "id": id,
-            "host": self.host,
-            "break_time": format_as_iso8601(self.break_time),
-            "location": self.location,
-            "holiday": self.holiday,
-            "cost": self.cost,
-            "host_reimbursed": format_as_maybe_iso8601(self.host_reimbursed),
-            "admin_claimed": format_as_maybe_iso8601(self.admin_claimed),
-            "admin_reimbursed": format_as_maybe_iso8601(self.admin_reimbursed),
-        }
-
 
 @dataclass
 class BreakFilters:
@@ -82,19 +44,14 @@ class BreakFilters:
     admin_claimed: Optional[bool] = None
     admin_reimbursed: Optional[bool] = None
 
-class ClaimConfig:
-    json_encoders = {Arrow: lambda x: x.isoformat()}
-    arbitrary_types_allowed = True
 
-@dataclass(config=ClaimConfig)
+@dataclass
 class Claim:
     id: int
     claim_date: Arrow
     breaks_claimed: List[Break]
     claim_amount: float
-    claim_reimbursed: Optional[Arrow] = field(
-        default=None, metadata=dict(example="2023-05-04T15:00")
-    )
+    claim_reimbursed: Optional[Arrow] = None
 
 
 def claim_list_date_string(breaks: List[Break]) -> str:
