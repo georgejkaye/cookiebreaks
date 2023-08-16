ed1566d23a093144bb385e1f87bc2eb96976f015
George Kaye <georgejkaye@gmail.com>
2023-08-16 23:34:38 +0100

    [fix] Fix times to show europe/london (for now...)

--- client/src/app/structs.ts
+++ client/src/app/structs.ts
@@ -1,7 +1,6 @@
 export const getDatetimeText = (datetime: Date) => {
-    let isoString = datetime.toISOString()
-    let date = isoString.substring(0, 10)
-    let time = isoString.substring(11, 16)
+    let date = getDateString(datetime)
+    let time = getTimeString(datetime)
     return `${date} at ${time}`
 }
 
@@ -19,28 +18,32 @@ export interface CookieBreak {
     success?: Date
 }
 
-export const getCookieBreakDate = (cb: CookieBreak) => {
-    let weekday = cb.datetime.toLocaleDateString("en-GB", {
+export const getDateString = (datetime: Date) => {
+    let weekday = datetime.toLocaleDateString("en-GB", {
         weekday: "long",
         timeZone: "Europe/London",
     })
-    let date = cb.datetime.toLocaleDateString("en-GB", {
+    let date = datetime.toLocaleDateString("en-GB", {
         day: "2-digit",
         timeZone: "Europe/London",
     })
-    let month = cb.datetime.toLocaleDateString("en-GB", {
+    let month = datetime.toLocaleDateString("en-GB", {
         month: "long",
         timeZone: "Europe/London",
     })
     return `${weekday} ${date} ${month}`
 }
-export const getCookieBreakTime = (cb: CookieBreak) => {
-    let time = cb.datetime.toLocaleTimeString("en-GB", {
+export const getTimeString = (datetime: Date) => {
+    let time = datetime.toLocaleTimeString("en-GB", {
         timeStyle: "short",
         timeZone: "Europe/London",
     })
     return time
 }
+export const getCookieBreakDate = (cb: CookieBreak) =>
+    getDateString(cb.datetime)
+export const getCookieBreakTime = (cb: CookieBreak) =>
+    getTimeString(cb.datetime)
 export const getFutureBreaks = (cbs: CookieBreak[]) =>
     // cbs.filter((cb) => cb.datetime.getTime() > Date.now())
     cbs
