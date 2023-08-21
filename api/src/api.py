42e20f37fa08c9b25f4214278b1ea0d62c0b48ee
George Kaye <georgejkaye@gmail.com>
2023-08-21 10:10:26 +0100

    [feat] Allow optional token to api methods

--- api/src/api.py
+++ api/src/api.py
@@ -121,7 +121,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token", auto_error=False)
 
 
 class Token(BaseModel):
@@ -179,24 +179,26 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str | None = payload.get("sub")
-        if username is None:
+async def get_current_user(token: Annotated[Optional[str], Depends(oauth2_scheme)]):
+    if token:
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+            username: str | None = payload.get("sub")
+            if username is None:
+                raise credentials_exception
+            token_data = TokenData(username=username)
+        except JWTError:
             raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(test_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+        user = get_user(test_users_db, username=token_data.username)
+        if user is None:
+            raise credentials_exception
+        return user
+    return None
 
 
 async def is_admin(current_user: Annotated[User, Depends(get_current_user)]):
