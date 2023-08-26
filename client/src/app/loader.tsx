5240042edcb6eb12e4c9f90e06b2843a7a7796c8
George Kaye <georgejkaye@gmail.com>
2023-08-26 16:29:29 +0100

    [feat] Add loader component

---
+++ client/src/app/loader.tsx
@@ -0,0 +1,7 @@
+import styles from "./loader.module.css"
+
+export const Loader = () => (
+    <div style={{ width: "25px", height: "25px" }} className={styles.loader} />
+)
+
+export default Loader
