ef27be0950b796368017e66ab70dafe00fefa1d0
George Kaye <georgejkaye@gmail.com>
2023-08-26 23:37:45 +0100

    [fix] Improve how loader parameters work

--- client/src/app/loader.tsx
+++ client/src/app/loader.tsx
@@ -2,12 +2,19 @@ import styles from "./loader.module.css"
 
 export const Loader = (props: { margin?: number; size?: number }) => {
     let size = `${props.size ? props.size : 25}px`
+    let margin = props.margin ? `m-${props.margin}` : ""
+    let colour = "#f3f3f3"
+    let colourSpin = "#3498db"
     return (
         <div
-            style={{ width: size, height: size }}
-            className={`${styles.loader} mx-auto my-${
-                props.margin ? props.margin : "0"
-            }`}
+            style={{
+                border: `${size} solid ${colour}`,
+                borderTop: `${size} solid ${colourSpin}`,
+                borderRadius: "50%",
+                width: size,
+                height: size,
+            }}
+            className={`${styles.loader} mx-auto`}
         />
     )
 }
