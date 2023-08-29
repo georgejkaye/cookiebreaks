74ceeb0674dd8a46ee2400569e0ca1d587edb2ba
George Kaye <georgejkaye@gmail.com>
2023-08-29 23:01:51 +0100

    [env] Add docker compose file

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/api/:path*",
-                destination: `${process.env.API || "http://localhost:8000"}/:path*`
+                destination: `${process.env.API_URL || "http://localhost:8000"}/:path*`
             }
         ]
     },
