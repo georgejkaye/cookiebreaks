0726f36487d446490cb30fd7652a724afb63f9f1
George Kaye <georgejkaye@gmail.com>
2023-09-19 00:05:10 +0100

    [fix] Make sure that mailctl always decrypts successfully

--- api/scripts/get_token.py
+++ api/scripts/get_token.py
@@ -1,10 +1,9 @@
 #!/bin/python
 # ~/.get_token.py
-import os
 from subprocess import check_output
 import subprocess
 
-subprocess.run(["cp", "mail/token.auth", "mail/oauth/token.auth"])
+subprocess.run(["cp", "mail/token.auth", "mail/oauth"])
 access_token = check_output(
     ["mailctl", "-c", "mail/config.yaml", "access", "token"]
 ).decode("utf-8")[:-1]
